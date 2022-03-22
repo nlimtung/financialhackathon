0008a4442aa1642ce2aad3b7d358a7ee07ead80e
Marianne <m.salamanca21@live.ca>
2022-03-22 16:30:16 -0400

    add profile page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.home, name = 'home'),
+    path('', views.home, name = 'home'),
     path('accounts/signup/', views.signup, name='signup'),
-    path ('habits/create/', views.HabitCreate.as_view(), name = 'habit_create'),
+    path('habits/create/', views.HabitCreate.as_view(), name = 'habit_create'),
     path('habits/<int:habit_id>/', views.habits_detail, name='detail'),
     path('habits/index/', views.habits_index, name='index'),
     path('habits/<int:pk>/delete/', views.HabitDelete.as_view(), name='habit_delete'),
     path('habits/<int:pk>/update/', views.habits_update, name='habit_update'),
-
+    path('profile/', views.profile, name = 'profile'),
 ] 
