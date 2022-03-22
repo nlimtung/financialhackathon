ad70b2f40bdcbee52859d13058b97a8e053dd411
Nicole <nicole@Nicoles-MacBook-Air.local>
2022-03-21 21:57:39 -0400

    adding  create  form

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+
+from django.views.generic.edit import CreateView
+from .models import Habit
+
 
 def home (request):
     return render  (request, "home.html") 
@@ -19,3 +24,16 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+class HabitCreate(CreateView):
+  model = Habit
+  fields = ['habit_item', 'habit_cost', 'item', 'item_cost']
+  def form_valid(self, form):
+    form.instance.user = self.request.user  
+    return super().form_valid(form)
+
+  # def form_valid(self, form):
+  #   form.instance.user = self.request.user  
+  #   return super().form_valid(form)
+  success_url = '/'
+
