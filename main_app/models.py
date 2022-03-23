7507c82644772f7fcd7fd372ef3612fcc0b7b2b4
Marianne <m.salamanca21@live.ca>
2022-03-22 22:16:53 -0400

    style create form

--- main_app/models.py
+++ main_app/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Habit (models.Model):
-    habit_item = models.CharField(max_length = 50)
-    habit_cost = models.FloatField("habit cost")
-    item = models.CharField(max_length = 50)
-    item_cost = models.FloatField("cost")
+    habit_item = models.CharField(verbose_name=u"NAME", max_length = 50, )
+    habit_cost = models.FloatField("PRICE")
+    item = models.CharField(verbose_name=u"HABIT NAME", max_length = 50)
+    item_cost = models.FloatField("ESTIMATED COST")
     initial_item_cost = models.FloatField("initial cost", null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     completed_goal = models.BooleanField(default= False)
@@ -16,6 +16,7 @@ class Habit (models.Model):
         return self.item
     def get_absolute_url(self):
         return reverse('detail', kwargs={'habit_id': self.id})
+
 
 
 
