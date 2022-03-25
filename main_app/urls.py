9a37cb52836d51b8097ff776098fb3f72bdd2298
Marianne <m.salamanca21@live.ca>
2022-03-24 21:27:56 -0400

    changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path('habits/<int:pk>/delete/', views.HabitDelete.as_view(), name='habit_delete'),
     path('habits/<int:pk>/update/', views.habits_update, name='habit_update'),
     path('profile/', views.profile, name = 'profile'),
-
-
     path('habits/completed', views.completed, name = 'completed'),
     path('profile/create/', views.ProfileCreate.as_view(), name = 'profile_create'),
     path('profile/<int:pk>/update', views.ProfileUpdate.as_view(), name = 'profile_update'),
